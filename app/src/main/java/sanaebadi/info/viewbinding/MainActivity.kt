c390ecc21bd5d47bc6a4be063852e8dee086f193
Sana Ebadi <sanaebadi97@gmail.com>
2020-03-05 12:55:29 -0500

    added first test for view binding

--- app/src/main/java/sanaebadi/info/viewbinding/MainActivity.kt
+++ app/src/main/java/sanaebadi/info/viewbinding/MainActivity.kt
@@ -2,11 +2,21 @@ package sanaebadi.info.viewbinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Toast
+import sanaebadi.info.viewbinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
+        binding.txtTest.setOnClickListener{
+            Toast.makeText(this,"HIIIII , ...." , Toast.LENGTH_SHORT).show()
+        }
     }
 }
